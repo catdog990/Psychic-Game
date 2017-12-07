ff3b4792c08af658a360d95aee8c0308827bfd60
catdog990 <ozmcastro@gmail.com>
2017-12-07 09:26:00 -0500

    almost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,21 @@ var computerChoices = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","
 	var wins = 0;
 	var losses = 0;
 	var ties = 0;
-	var GuessesLeft = 9;
+	var GuessesLeft = 15;
 	var currentGuess = 0;
-//***function runs whenever a key is pressed***///
-if(gameOver == false){
-	document.onkeyup = function(event){
 
-		var userGuess = event.key;
 
-//***might need to go above onkeyup**//
+
+	 document.onkeyup = function(event) {
+
+     
+      var userGuess = event.key;
+      console.log(userGuess);
+
+
 		var computerGuess = computerChoices[Math.floor(Math. random() * computerChoices.length)];
+		console.log(computerGuess);
+		
 
 		if((userGuess === computerGuess)){
 			wins++;
@@ -21,19 +26,30 @@ if(gameOver == false){
 			GuessesLeft--;
 		}
 		
-		if((GuessesLeft = 0)){
-			gameOver=true;
+		if((GuessesLeft === 0)){
+			gameOver===true;
 		}
 
-		if((gameOver)){
+		if((gameOver=== true)){
 			losses++
-			GuessesLeft===9
+			GuessesLeft=== 9;
 		}
 
+var html =
+          "<p>Wins: " + wins + "</p>" +
+          "<p>Losses: " + losses + "</p>" +
+          "<p>Ties: " + ties + "</p>" +
+          "<p>Guesses-Left: " + GuessesLeft + "</p>";
+         
 
-		}
+			document.querySelector("#wut").innerHTML = html;
 
-	}
+
+
+
+		};
+
+	
 
 ///write span and give it an id, then var whatever = document.getElementbyID(id of span);
 //then whatever.innerHtml = userGuess
